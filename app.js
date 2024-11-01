00a6bca2f288951554542badb5f12b5cbd524a7a
Raikou320 <duchaussoytheo@outlook.fr>
2024-11-01 20:30:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 class App {
-  constructor(config) {
-    this.configPhysics = config.physics;
-    this.gravityY = config.physics.gravity.y;
-    this.gravityX = config.physics.gravity.x;
-  }
-  physics() {
-    gravity = {
-      x: this.gravityX,
-      y: this.gravityY,
+  constructor() {
+    this.add = {
+      sprite: (width, height, x, y, color) => {
+        const div = document.createElement('div');
+        div.style.width = `${width}px`;
+        div.style.height = `${height}px`;
+        div.style.position = 'absolute';
+        div.style.left = `${x}px`;
+        div.style.bottom = `${y}px`;
+        div.style.backgroundColor = color;
+        document.body.appendChild(div);
+      },
     };
   }
 }
